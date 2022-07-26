c73cd23cd3b3793cf46168c2f5f8c7b93c9f5630
Mano215900 <bujjulamanoharreddy7@gmail.com>
2022-07-26 21:48:36 +0530

    Employee Details
    Commited Employee details

---
+++ MVCFirstProject/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using MVCFirstProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFirstProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult EmployeeInfo()
+        {
+            
+                Employee employee = new Employee()
+                { 
+               
+                    EmployeeId = 215900,
+                    EmployeeName = "Manohar",
+                    EmployeeLocation = "Bengalore"
+
+                };
+
+                return View(employee);
+        }
+    }
+}
